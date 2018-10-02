d5237e86d6a8c39c67ebb4ca70535bd36bc4b1db
RukkiesMan <pavelekseev@gmail.com>
2018-10-02 20:53:54 +0300

    add break line in greeting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export const askUserName = () => {
   const actual = readlineSync.question('\nMay I have your name? ');
-  console.log(`Hello, ${actual}!`);
+  console.log(`Hello, ${actual}!\n`);
 };
 
 export const getRandomNum = maxNumber => Math.floor(Math.random() * Math.floor(maxNumber));
