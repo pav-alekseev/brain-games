a4d91479ae76ac65b8b800fe90229a6169e1cab9
RukkiesMan <pavelekseev@gmail.com>
2018-10-02 21:56:11 +0300

    add play game function

--- src/index.js
+++ src/index.js
@@ -9,3 +9,22 @@ export const getRandomNum = maxNumber => Math.floor(Math.random() * Math.floor(m
 export const showQuestionNumber = number => console.log(`Question: ${number}`);
 
 export const askUserAnswer = () => readlineSync.question('Your answer: ');
+
+export const playGame = (userName) => {
+  function iter(currentLap) {
+    if (currentLap === 3) {
+      return `Congratulations, ${userName}!`;
+    }
+    const number = getRandomNum(20);
+    showQuestionNumber(number);
+    const isEvenNumber = (number % 2 === 0) ? 'yes' : 'no';
+    const answer = askUserAnswer(number);
+    if (isEvenNumber === answer) {
+      console.log('Correct!');
+      return iter(currentLap + 1);
+    }
+    const correctAnswer = (answer === 'yes' ? 'no' : 'yes');
+    return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
+  }
+  return iter(0);
+};
