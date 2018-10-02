9a7a5ac39e4454f6364f23f5872f596f4ed3a9d7
RukkiesMan <pavelekseev@gmail.com>
2018-10-02 20:43:52 +0300

    change export settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const askUserName = () => {
   const actual = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${actual}!`);
 };
