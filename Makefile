f2f437c1116902eab4571d48b5887bb20d959e22
RukkiesMan <pavelekseev@gmail.com>
2018-10-04 20:58:49 +0300

    add brain progression game

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npx babel-node -- src/bin/brain-gcd.js
+	npx babel-node -- src/bin/brain-progression.js
 
 publish:
 	npm publish
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,5 +1,29 @@
+import { cons } from 'hexlet-pairs';
 import playGame from '..';
+import getRandomNumber from '../utils';
 
-const gameIteration = () => {};
+const maxBeginOfProgression = 10;
+const maxNumberOfSteps = 10;
+const lengthOfProgression = 10;
+
+const makeProgression = (currentNumber, step, currentLength, questionStep) => {
+  if (currentLength === lengthOfProgression) {
+    return currentNumber;
+  }
+  const nextNumber = currentNumber + step;
+  if (currentLength === questionStep) {
+    return `.. ${makeProgression(nextNumber, step, currentLength + 1, questionStep)}`;
+  }
+  return `${currentNumber} ${makeProgression(nextNumber, step, currentLength + 1, questionStep)}`;
+};
+
+const gameIteration = () => {
+  const beginOfProgression = getRandomNumber(maxBeginOfProgression);
+  const step = getRandomNumber(maxNumberOfSteps);
+  const questionStep = getRandomNumber(lengthOfProgression);
+  const question = makeProgression(beginOfProgression, step, 0, questionStep);
+  const rightAnswer = (beginOfProgression + step * questionStep).toString();
+  return cons(question, rightAnswer);
+};
 
 export default () => playGame('What number is missing in this progression?', gameIteration);
