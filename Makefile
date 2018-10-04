eb6364a6d9f6aa6a7653db2c4a0f20c7616d35d6
RukkiesMan <pavelekseev@gmail.com>
2018-10-04 10:40:04 +0300

    add brain gcd game logic

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npx babel-node -- src/bin/brain-calc.js
+	npx babel-node -- src/bin/brain-gcd.js
 
 publish:
 	npm publish
--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,17 +1,30 @@
 import {
-  maxRandomNumber, showGameGreeting, showUserGreeting, showQuestion, showResult, getUserName,
-  getUserAnswer, getRandomNumber, gameLoop, isRightUserAnswer, isEven,
+  showGameGreeting, showUserGreeting, showQuestion, showResult, getUserName, getUserAnswer,
+  getRandomNumber, gameLoop, isRightUserAnswer,
 } from '..';
 
 const maxNumber = 100;
 
-const getGCD
+const getGCD = (firstNumber, secondNumber) => {
+  if (firstNumber === 0 && secondNumber === 0) {
+    return 0;
+  }
+  if (firstNumber === secondNumber) {
+    return firstNumber;
+  }
+  if (firstNumber > secondNumber) {
+    return getGCD(firstNumber - secondNumber, secondNumber);
+  }
+  return getGCD(firstNumber, secondNumber - firstNumber);
+};
 
 const gameIteration = () => {
   const firstNumber = getRandomNumber(maxNumber);
   const secondNumber = getRandomNumber(maxNumber);
   showQuestion(`${firstNumber} ${secondNumber}`);
-
+  const rightAnswer = getGCD(firstNumber, secondNumber);
+  const userAnswer = Number(getUserAnswer());
+  return isRightUserAnswer(rightAnswer, userAnswer);
 };
 
 export default () => {
