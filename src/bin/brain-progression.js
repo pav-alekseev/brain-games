0b4adc083e6d4e75c53701e649df57c352cfb8bd
RukkiesMan <pavelekseev@gmail.com>
2018-10-04 11:56:12 +0300

    add brain progression game

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playBrainProgression from '../games/brain-progression';
+
+playBrainProgression();
---
+++ src/games/brain-progression.js
@@ -0,0 +1,5 @@
+import { playGame } from '..';
+
+const gameIteration = () => {};
+
+export default () => playGame('What number is missing in this progression?', gameIteration);
