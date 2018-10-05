ae1dd0133684c25af35009a51f1ca4e3d996a487
RukkiesMan <pavelekseev@gmail.com>
2018-10-05 13:03:23 +0300

    add brain prime game

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 start:
-	npx babel-node -- src/bin/brain-progression.js
+	npx babel-node -- src/bin/brain-prime.js
 
 publish:
 	npm publish
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,2 +1,28 @@
-export default () => {
+import { cons } from 'hexlet-pairs';
+import playGame from '..';
+import getRandomNumber from '../utils';
+
+const maxNumber = 1000;
+const firstPrimeNumber = 2;
+
+const isPrime = (number) => {
+  const iter = (divider) => {
+    if (number % divider === 0) {
+      return false;
+    }
+    if (divider > number / 2) {
+      return true;
+    }
+    return iter(divider + 1);
+  };
+  return iter(firstPrimeNumber);
 };
+
+const gameIteration = () => {
+  const question = getRandomNumber(firstPrimeNumber, maxNumber);
+  const rightAnswer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, rightAnswer);
+};
+
+export default () => playGame('Answer "yes" if given number is prime. Otherwise answer "no".',
+  gameIteration);
